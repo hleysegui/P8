e0624b29829e53e6be08ba12a03b3d64c0f2232f
ashley segui <ashley.segui@icloud.com>
2024-02-09 06:22:23 +0100

    create about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,10 +1,40 @@
+import { useEffect, useState } from "react"
 import Banner from "../../components/reusable/Banner"
 import bannerAbout from '/src/assets/images/banner-about.png'
+import Accordion from "../../components/Accordion"
+import { Exception } from "sass"
 
 function About() {
+
+    const [faqs, setFaqs] = useState([])
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await fetch('../data/about.json')
+                const data = await response.json()
+                setFaqs(data)
+
+            }catch(err) {
+                throw new Exception(err)
+            }
+        }
+        fetchData()
+    }, [])
+
     return (
         <main>
-            <Banner image={ bannerAbout } text=""/>
+            <div className="about">
+                <Banner image={ bannerAbout } text=""/>
+                <div className="accordion__content">
+                    {faqs.map((faq, index) => (
+                        <Accordion key={`${faq}-${index}`}
+                        header={faq.title}
+                        body={faq.content}
+                        />
+                    ))}
+                </div>
+            </div>
         </main>
     )
 }
