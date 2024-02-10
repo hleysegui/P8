24150d187a6248723c3cd49edc2b097b038372c8
ashley segui <ashley.segui@icloud.com>
2024-02-10 07:01:41 +0100

    change structure for routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router"
 import About from "./pages/About/About"
 import Home from "./pages/Home/Home"
 import Error404 from "./pages/Error/Error404"
+import Logement from "./pages/Logement/Logement"
 import { APIContextProvider } from "./services/apiContext"
  
 function App() {
@@ -11,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/logement/:id" element={<Fiche />} />
+        <Route path="/logement/:id" element={<Logement />} />
         <Route path="*" element={<Error404 />} />
       </Routes>
     </APIContextProvider>
